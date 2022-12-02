c428d73041a4c4b3f1eb697f3e78de5b1c7a1669
Pakenfit <mamadembele7@gmail.com>
2022-12-02 01:32:25 +0100

    fix(build): Fix tests and npm publish

--- __tests__/cli-integration.test.ts
+++ __tests__/cli-integration.test.ts
@@ -1,31 +1,29 @@
-import { system, filesystem } from 'gluegun'
+import { system, filesystem } from 'gluegun';
 
-const src = filesystem.path(__dirname, '..')
+const src = filesystem.path(__dirname, '..');
 
-const cli = async (cmd) =>
-  system.run(
-    'node ' + filesystem.path(src, 'bin', 'pakenative-cli') + ` ${cmd}`
-  )
+const cli = async cmd =>
+  system.run('node ' + filesystem.path(src, 'bin', 'pakenative-cli') + ` ${cmd}`);
 
 test('outputs version', async () => {
-  const output = await cli('--version')
-  expect(output).toContain('0.0.1')
-})
+  const output = await cli('--version');
+  expect(output).toContain('Welcome to pakenative CLI version 1.0.0');
+});
 
 test('outputs help', async () => {
-  const output = await cli('--help')
-  expect(output).toContain('0.0.1')
-})
+  const output = await cli('--help');
+  expect(output).toContain('1.0.0');
+});
 
 test('generates file', async () => {
-  const output = await cli('generate foo')
+  const output = await cli('generate foo');
 
-  expect(output).toContain('Generated file at models/foo-model.ts')
-  const foomodel = filesystem.read('models/foo-model.ts')
+  expect(output).toContain('Generated file at models/foo-model.ts');
+  const foomodel = filesystem.read('models/foo-model.ts');
 
-  expect(foomodel).toContain(`module.exports = {`)
-  expect(foomodel).toContain(`name: 'foo'`)
+  expect(foomodel).toContain(`module.exports = {`);
+  expect(foomodel).toContain(`name: 'foo'`);
 
   // cleanup artifact
-  filesystem.remove('models')
-})
+  filesystem.remove('models');
+});
